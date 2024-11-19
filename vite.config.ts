56ff6d35fb7469e171c6d20d509d39b85dd74126
Lucas <joaolucascp16@gmail.com>
2024-11-19 10:52:11 -0300

    Update TypeScript configuration and add custom Vite plugin

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,7 @@
-import { defineConfig } from 'vite';
-import tsconfigPaths from 'vite-tsconfig-paths';
+import { defineConfig } from "vite"
+import tsconfigPaths from "vite-tsconfig-paths"
+import jonLoader from "./plugins/vite-plugin-jon"
 
 export default defineConfig({
-  plugins: [tsconfigPaths()],
-});
+  plugins: [tsconfigPaths(), jonLoader()],
+})
